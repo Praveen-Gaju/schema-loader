1d03cfdd83bf8b2f383416fee8aae3d5125b8f55
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-09-21 20:21:00 +0530

    updating schema-loader directory

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM            dokken/centos-8
 RUN             yum install epel-release -y
 COPY            mongo.repo /etc/yum.repos.d/mongo.repo
 RUN             yum install unzip jq git mysql mongodb-org-shell -y
+RUN             curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install
 COPY            run.sh /
 ENTRYPOINT      [ "bash", "/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,2 +1,29 @@
-echo First Argument - $1
-echo second Argumernt = $2
+## creating /app folder and doing schema setup as per requirement
+git clone https://github.com/Praveen-Gaju/$2.git /app
+cd /app
+
+## schema types mongo and mysql
+case $1 in
+  mongo)
+    curl -L https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem -o /app/rds-combined-ca-bundle.pem
+    mongo --ssl \
+    --host $(aws ssm get-parameter --name ${env}.docdb.endpoint --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g'):27017 \
+    --sslCAFile /app/rds-combined-ca-bundle.pem \
+    --username $(aws ssm get-parameter --name ${env}.docdb.user --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+    --password $(aws ssm get-parameter --name ${env}.docdb.password --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') </app/schema/${2}.js
+    ;;
+  mysql)
+    mysql -h $(aws ssm get-parameter --name ${env}.rds.endpoint --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+    -u$(aws ssm get-parameter --name ${env}.rds.user --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+    -p$(aws ssm get-parameter --name ${env}.rds.password --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') < /app/schema/${2}.sql
+    ;;
+  *)
+    echo schema loading supported only for mongo and mysql
+    exit 1
+    ;;
+esac
+
+
+      name: Check Schema
+      ansible.builtin.shell: echo 'show databases' | mysql -h {{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.endpoint', region='us-east-1') }} -u{{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.user', region='us-east-1') }} -p{{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.password', region='us-east-1') }} | grep cities
+      register: schema_status
